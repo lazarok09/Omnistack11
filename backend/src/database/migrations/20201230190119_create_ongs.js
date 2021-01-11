affaf8a4a93fcced93938f75d018c24bdef81068
lazarok09 <lazarok09@gmail.com>
2021-01-11 14:16:37 -0300

    1.0

--- backend/src/database/migrations/20201230190119_create_ongs.js
+++ backend/src/database/migrations/20201230190119_create_ongs.js
@@ -1,18 +1,16 @@
-
-exports.up = function(knex) {
+exports.up = function (knex) {
   /* criando tabelas pela documentação do knex */
-  return knex.schema.createTable('ongs', function(table) {
-      table.string('id').primary();
-      table.string('name').notNullable();
-      table.string('email').notNullable();
-      table.string('whatsapp').notNullable();
-      table.string('city').notNullable();
-      table.string('uf', 2).notNullable();
-
-  })
+  return knex.schema.createTable("ongs", function (table) {
+    table.string("id").primary();
+    table.string("name").notNullable();
+    table.string("email").notNullable();
+    table.string("whatsapp").notNullable();
+    table.string("city").notNullable();
+    table.string("uf", 2).notNullable();
+  });
 };
 // caso der errado apagar a tabela
 
-exports.down = function(knex) {
-  return knex.schema.dropTable('ongs')
+exports.down = function (knex) {
+  return knex.schema.dropTable("ongs");
 };
